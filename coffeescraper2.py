d3d08df1c238ad8a587333117baf6ce8168453e9
dominikacecylia <dominikacecylia@users.noreply.github.com>
2021-05-09 21:27:00 +0100

    Scrapes rest of coffee details such as roast, grind etc.

--- coffeescraper2.py
+++ coffeescraper2.py
@@ -21,7 +21,6 @@ class CoffeeScraper2:
     
     def _get_coffee_details(self):
         img_url = self.driver.find_element_by_xpath('/html/body/div[5]/div/div[2]/div/div/div[3]//img').get_attribute('src')
-        #                                           /html/body/div[5]/div/div[2]/div/div/div[3]/div[1]/div[1]/a/img
         price = self.driver.find_element_by_xpath('/html/body/div[5]/div/div[2]/div/div/div[3]/div[3]/div[1]/div[1]/span[3]').text
 
         description_elements = self.driver.find_elements_by_xpath('//*[@id="description"]/span/div[1]')
@@ -29,12 +28,25 @@ class CoffeeScraper2:
         for element in description_elements:
             description.append(element.text)
 
-        # print(price)
-        return img_url, price, description
+        details_button = self.driver.find_element_by_xpath('//*[@id="tabs"]/ul/li[2]/a/span')
+        self.driver.execute_script("arguments[0].click();", details_button)
+        time.sleep(1)
+        
+        details = self.driver.find_elements_by_xpath('//*[@id="attributes"]/table')
+
+        coffee_details = []
+        for detail in details:
+            coffee_details.append(detail.text)
+        # process = self.driver.find_element_by_xpath('//*[@id="attributes"]/table/tbody/tr[6]/td[2]').text
+        # grind_types = self.driver.find_element_by_xpath('//*[@id="attributes"]/table/tbody/tr[8]/td[2]').text
+        # roast = self.driver.find_element_by_xpath('//*[@id="attributes"]/table/tbody/tr[7]/td[2]').text
+        # //*[@id="attributes"]/table/tbody/tr[7]/td[2]
+        # blend = self.driver.find_element_by_xpath('//*[@id="attributes"]/table/tbody/tr[5]/td[2]').text
+
+        return img_url, price, description, coffee_details
 
     def scrape(self):
         self.search()
-        # coffees = self.driver.find_elements_by_xpath('/html/body/div[5]/div/div[2]/div[4]') #issue as not a list
         coffees = self.driver.find_elements_by_xpath('//div[@class="products-list"]//a')
         print(len(coffees))
 
@@ -50,7 +62,7 @@ class CoffeeScraper2:
             self.driver.get(link)
             
 
-            img_url, price, description = self._get_coffee_details()
+            img_url, price, description, coffee_details = self._get_coffee_details()
 
             if '.png' in img_url:
                 ext = 'png'
@@ -61,6 +73,11 @@ class CoffeeScraper2:
             detail = {
                 'img_url' : img_url,
                 'price' : price,
+                'coffee_details' : coffee_details,
+                # 'process' : process,
+                # 'grind_types' : grind_types,
+                # 'flavor' : flavor,
+                # 'roast' : roast,
                 'description' : description
             }
             details.append(detail)
