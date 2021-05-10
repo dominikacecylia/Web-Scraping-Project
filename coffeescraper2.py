2f5d22d7cedbdf97bcc58851083b49f28f00bd17
dominikacecylia <dominikacecylia@users.noreply.github.com>
2021-05-10 15:54:50 +0100

    Optimizes code

--- coffeescraper2.py
+++ coffeescraper2.py
@@ -16,6 +16,7 @@ class CoffeeScraper2:
         self.driver = webdriver.Chrome()
 
     def search(self):
+        # self.driver.get('https://www.coffeedesk.pl/kawa/filters/on-page/120/')
         self.driver.get('https://www.coffeedesk.pl/kawa/')
         
     
@@ -23,30 +24,17 @@ class CoffeeScraper2:
         img_url = self.driver.find_element_by_xpath('/html/body/div[5]/div/div[2]/div/div/div[3]//img').get_attribute('src')
         price = self.driver.find_element_by_xpath('/html/body/div[5]/div/div[2]/div/div/div[3]/div[3]/div[1]/div[1]/span[3]').text
 
-        description_elements = self.driver.find_elements_by_xpath('//*[@id="description"]/span/div[1]')
-        description = []
-        for element in description_elements:
-            description.append(element.text)
-
-        details_button = self.driver.find_element_by_xpath('//*[@id="tabs"]/ul/li[2]/a/span')
-        self.driver.execute_script("arguments[0].click();", details_button)
-        time.sleep(1)
+        description = self._get_details_in_table('//*[@id="description"]/span/div[1]')
+        self._click_on_button('//*[@id="tabs"]/ul/li[2]/a/span')
         
-        details = self.driver.find_elements_by_xpath('//*[@id="attributes"]/table')
-
-        coffee_details = []
-        for detail in details:
-            coffee_details.append(detail.text)
-        # process = self.driver.find_element_by_xpath('//*[@id="attributes"]/table/tbody/tr[6]/td[2]').text
-        # grind_types = self.driver.find_element_by_xpath('//*[@id="attributes"]/table/tbody/tr[8]/td[2]').text
-        # roast = self.driver.find_element_by_xpath('//*[@id="attributes"]/table/tbody/tr[7]/td[2]').text
-        # //*[@id="attributes"]/table/tbody/tr[7]/td[2]
-        # blend = self.driver.find_element_by_xpath('//*[@id="attributes"]/table/tbody/tr[5]/td[2]').text
+        coffee_details = self._get_details_in_table('//*[@id="attributes"]/table')
 
         return img_url, price, description, coffee_details
 
     def scrape(self):
         self.search()
+        """Tutaj skonfigurowac petle do """
+
         coffees = self.driver.find_elements_by_xpath('//div[@class="products-list"]//a')
         print(len(coffees))
 
@@ -68,31 +56,41 @@ class CoffeeScraper2:
                 ext = 'png'
             else:
                 ext = 'jpg'
-            self.download_file(img_url, f'data/pictures/c2/kawa_{idx}.{ext}')
+            self._download_file(img_url, f'data/pictures/c2/kawa_{idx}.{ext}')
 
             detail = {
                 'img_url' : img_url,
                 'price' : price,
                 'coffee_details' : coffee_details,
-                # 'process' : process,
-                # 'grind_types' : grind_types,
-                # 'flavor' : flavor,
-                # 'roast' : roast,
                 'description' : description
             }
             details.append(detail)
 
-        with open('data/data_pl_coffee.json', 'w') as f:
+        with open('data/data_pl_coffee_test.json', 'w') as f:
             json.dump(details, f, indent=4)
 
 
     def scroll(self, x=0, y=10000):
         self.driver.execute_script(f'window.scrollBy({x}, {y})')
 
-    def download_file(self, src_url, local_destination):
+    def _download_file(self, src_url, local_destination):
         response = requests.get(src_url)
         with open(local_destination, 'wb+') as f:
             f.write(response.content)
+
+    def _click_on_button(self, xpath):
+        button = self.driver.find_element_by_xpath(xpath)
+        self.driver.execute_script("arguments[0].click();", button)
+        time.sleep(1)
+    
+    def _get_details_in_table(self, xpath):
+        details = self.driver.find_elements_by_xpath(xpath)
+        all_details = [detail.text for detail in details]
+        return all_details
+
+
+
+
 
 
 scraper2 = CoffeeScraper2()
