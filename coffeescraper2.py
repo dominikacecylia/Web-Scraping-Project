af0f216e362cbbb0c2d7fdd8c820cdfc7343f059
dominikacecylia <dominikacecylia@users.noreply.github.com>
2021-05-11 14:36:46 +0100

    Fixes dictionary bug and duplicates in urls

--- coffeescraper2.py
+++ coffeescraper2.py
@@ -7,13 +7,17 @@ import time
 """Try:
     - storing data in csv
     - try and catch for exception of lacking details on particular products
-    - 
+    - use coffee name as unique coffee id and then when adding it check if its not in the list already/file 
 """
 
 class CoffeeScraper2:
     
     def __init__(self):
         self.driver = webdriver.Chrome()
+        self.already_visited_links = [] #read the json file to add scraped links, 
+        # with open('name of my json file', 'r') as f:
+            # pass
+        #     # TODO: list comprehension to get the list of unique ids
 
     def search(self, url):
         self.driver.get(url)
@@ -22,33 +26,34 @@ class CoffeeScraper2:
     def _get_coffee_details(self):
         img_url = self.driver.find_element_by_xpath('/html/body/div[5]/div/div[2]/div/div/div[3]//img').get_attribute('src')
         price = self.driver.find_element_by_xpath('/html/body/div[5]/div/div[2]/div/div/div[3]/div[3]/div[1]/div[1]/span[3]').text
-        description = self._get_details_in_table('//*[@id="description"]/span/div[1]')
+        description = [desc.text for desc in self.driver.find_elements_by_xpath('//*[@id="description"]/span/div[1]')] # TODO: change the function here
 
         self._click_on_button('//*[@id="tabs"]/ul/li[2]/a/span')
         
-        coffee_details = self._get_details_in_table('//*[@id="attributes"]/table')
+        coffee_details = self._get_details_in_table('//*[@id="attributes"]/table/tbody')
         return img_url, price, description, coffee_details
 
     def scrape(self):
         urls = ('https://www.coffeedesk.pl/kawa/filters/on-page/120/0/','https://www.coffeedesk.pl/kawa/filters/on-page/120/1/') 
-        #'https://www.coffeedesk.pl/kawa/filters/on-page/120/2/') #,'https://www.coffeedesk.pl/kawa/filters/on-page/120/3/'
-        # 'https://www.coffeedesk.pl/kawa/filters/on-page/120/4/','https://www.coffeedesk.pl/kawa/filters/on-page/120/5/'#
-        # 'https://www.coffeedesk.pl/kawa/filters/on-page/120/6/','https://www.coffeedesk.pl/kawa/filters/on-page/120/7/')
+        # urls = ('https://www.coffeedesk.pl/kawa/filters/on-page/120/2/','https://www.coffeedesk.pl/kawa/filters/on-page/120/3/')
+        # urls = ('https://www.coffeedesk.pl/kawa/filters/on-page/120/4/','https://www.coffeedesk.pl/kawa/filters/on-page/120/5/')
+        # urls = ('https://www.coffeedesk.pl/kawa/filters/on-page/120/6/','https://www.coffeedesk.pl/kawa/filters/on-page/120/7/')
         for url in urls:
             self.search(url)
 
             coffees = self.driver.find_elements_by_xpath('//div[@class="products-list"]//a')
             print(len(coffees))
 
-            links = [coffee.get_attribute('href') for coffee in coffees]
-            # for coffee in coffees:
-            #     link = coffee.get_attribute('href')
-            #     links.append(link)
-                
+            links = [coffee.get_attribute('href') for coffee in coffees ]                
 
             details = []
             for idx, link in enumerate(links):
-
+                if link in self.already_visited_links:
+                    print("Already visited this link")
+                    continue
+                else:
+                    self.already_visited_links.append(link)
+                
                 self.driver.get(link)
                 img_url, price, description, coffee_details = self._get_coffee_details()
 
@@ -59,14 +64,15 @@ class CoffeeScraper2:
                 self._download_file(img_url, f'data/pictures/c2/kawa_{idx}.{ext}')
 
                 detail = {
+                    'unique_id' : link,
                     'img_url' : img_url,
                     'price' : price,
-                    'coffee_details' : coffee_details,
+                    'coffee_details' : coffee_details, # TODO: manipulate the dictionary to get the kv pairs directly instead of dict inside dict
                     'description' : description
                 }
                 details.append(detail)
 
-            with open('data/data_pl_coffee_part1.json', 'w') as f:
+            with open('data/data_pl_coffee_part1_test.json', 'w') as f:
                 json.dump(details, f, indent=4)
 
 
@@ -84,8 +90,12 @@ class CoffeeScraper2:
         time.sleep(1)
     
     def _get_details_in_table(self, xpath):
-        details = self.driver.find_elements_by_xpath(xpath)
-        all_details = [detail.text for detail in details]
+        details = self.driver.find_element_by_xpath(xpath)
+        rows = details.find_elements_by_tag_name('tr')
+        all_details = {}
+        for row in rows:
+            key, value = [e.text for e in row.find_elements_by_tag_name('td')] # TODO: try prints, try doule nesting maybe, and have try and catch
+            all_details[key] = value
         return all_details
 
 
